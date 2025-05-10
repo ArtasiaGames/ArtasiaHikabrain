21d4cec4a0cb209bd8e50224b93fee040fbe0951
CapelleGab <gabin.capelle.pro@gmail.com>
2025-05-10 14:06:08 +0200

    REFACTOR InteractListener.java

--- src/main/java/fr/zeloxeuh/artasiaHikabrain/listener/player/InteractListener.java
+++ src/main/java/fr/zeloxeuh/artasiaHikabrain/listener/player/InteractListener.java
@@ -1,7 +1,7 @@
-package fr.zeloxeuh.artasiaHikabrain.Listener.Player;
+package fr.zeloxeuh.artasiaHikabrain.listener.player;
 
-import fr.zeloxeuh.artasiaHikabrain.ArtasiaHikabrain;
-import fr.zeloxeuh.artasiaHikabrain.Manager.GameManager;
+import fr.zeloxeuh.artasiaHikabrain.builder.ItemBuilder;
+import fr.zeloxeuh.artasiaHikabrain.manager.GameManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -13,17 +13,13 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class InteractListener implements Listener {
+    private final GameManager gameManager;
 
-    private ArtasiaHikabrain instance;
-    private GameManager gameManager;
-
-    public InteractListener(ArtasiaHikabrain instance, GameManager gameManager) {
-        this.instance = instance;
+    public InteractListener( GameManager gameManager) {
         this.gameManager = gameManager;
     }
 
@@ -33,28 +29,21 @@ public class InteractListener implements Listener {
         Action action = event.getAction();
         ItemStack item = event.getItem();
 
-        if (item == null) {
-            return;
-        }
-        if (item.getType().equals(Material.COMPASS) && item.hasItemMeta() && item.getItemMeta().getDisplayName().equals("§6Teams Selection")) {
+        if (item == null) return;
+
+        if (item.getType().equals(Material.COMPASS) && item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).getDisplayName().equals("§6Teams Selection")) {
             if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
                 Inventory inv = Bukkit.createInventory(null, 27, "§8Teams");
 
-                // Red Team item
-                ItemStack redTeam = new ItemStack(Material.RED_WOOL);
-                ItemMeta redMeta = redTeam.getItemMeta();
-                assert redMeta != null;
-                redMeta.setDisplayName("§cRed Team");
-                redMeta.setLore(List.of("§7This team will be red."));
-                redTeam.setItemMeta(redMeta);
+                ItemStack redTeam =  new ItemBuilder(Material.RED_WOOL)
+                        .setName("§cRed Team")
+                        .setLore("§7This team will be red.")
+                        .build();
 
-                // Blue Team item
-                ItemStack blueTeam = new ItemStack(Material.BLUE_WOOL);
-                ItemMeta blueMeta = blueTeam.getItemMeta();
-                assert blueMeta != null;
-                blueMeta.setDisplayName("§9Blue Team");
-                blueMeta.setLore(List.of("§7This team will be blue."));
-                blueTeam.setItemMeta(blueMeta);
+                ItemStack blueTeam = new ItemBuilder(Material.BLUE_WOOL)
+                        .setName("§9Blue Team")
+                        .setLore("§7This team will be blue.")
+                        .build();
 
                 // Set Team item in inventory
                 inv.setItem(12, new ItemStack(redTeam));
@@ -66,9 +55,7 @@ public class InteractListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
-        Inventory inv = event.getInventory();
         String invTitle = event.getView().getTitle();
-
         Player player = (Player) event.getWhoClicked();
         ItemStack item = event.getCurrentItem();
 
@@ -80,55 +67,42 @@ public class InteractListener implements Listener {
 
             switch (item.getType()) {
                 case RED_WOOL:
-                    // Retirer le joueur de son équipe actuelle si nécessaire
+                    // Retirer le joueur de son équipe actuelle si nécessaire et l'ajouter à l'équipe rouge
                     if (gameManager.getBlueTeam().getPlayers().contains(player)) {
                         gameManager.getBlueTeam().removePlayer(player);
                     }
-                    
-                    // Ajouter le joueur à l'équipe rouge
                     if (!gameManager.getRedTeam().getPlayers().contains(player)) {
                         gameManager.getRedTeam().addPlayer(player);
                     }
-                    
-                    // Donner l'item de laine rouge
-                    ItemStack redWoolItem = new ItemStack(Material.RED_WOOL, 1);
-                    ItemMeta redWoolMeta = redWoolItem.getItemMeta();
-                    if (redWoolMeta != null) {
-                        redWoolMeta.setDisplayName(gameManager.getRedTeam().getColor() + "Équipe " + gameManager.getRedTeam().getName());
-                        redWoolMeta.setLore(List.of("§7Vous êtes dans l'équipe rouge"));
-                        redWoolItem.setItemMeta(redWoolMeta);
-                    }
-                    
+
                     // Placer l'item dans la hotbar du joueur
+                    ItemStack redWoolItem = new ItemBuilder(Material.RED_WOOL)
+                            .setName(gameManager.getRedTeam().getColor() + "Équipe " + gameManager.getRedTeam().getName())
+                            .setLore("§7Vous êtes dans l'équipe rouge")
+                            .build();
                     player.getInventory().setItem(0, redWoolItem);
                     player.updateInventory();
-                    
+
+
                     // Message de confirmation
                     player.sendMessage("§7[§eHikaBrain§7] §fVous avez rejoint l'équipe " + 
                             gameManager.getRedTeam().getColor() + gameManager.getRedTeam().getName());
                     break;
                     
                 case BLUE_WOOL:
-                    // Retirer le joueur de son équipe actuelle si nécessaire
+                    // Retirer le joueur de son équipe actuelle si nécessaire et l'ajouter à l'équipe bleu
                     if (gameManager.getRedTeam().getPlayers().contains(player)) {
                         gameManager.getRedTeam().removePlayer(player);
                     }
-                    
-                    // Ajouter le joueur à l'équipe bleue
                     if (!gameManager.getBlueTeam().getPlayers().contains(player)) {
                         gameManager.getBlueTeam().addPlayer(player);
                     }
-                    
-                    // Donner l'item de laine bleue
-                    ItemStack blueWoolItem = new ItemStack(Material.BLUE_WOOL, 1);
-                    ItemMeta blueWoolMeta = blueWoolItem.getItemMeta();
-                    if (blueWoolMeta != null) {
-                        blueWoolMeta.setDisplayName(gameManager.getBlueTeam().getColor() + "Équipe " + gameManager.getBlueTeam().getName());
-                        blueWoolMeta.setLore(List.of("§7Vous êtes dans l'équipe bleue"));
-                        blueWoolItem.setItemMeta(blueWoolMeta);
-                    }
-                    
+
                     // Placer l'item dans la hotbar du joueur
+                    ItemStack blueWoolItem = new ItemBuilder(Material.BLUE_WOOL)
+                            .setName(gameManager.getBlueTeam().getColor() + "Équipe " + gameManager.getBlueTeam().getName())
+                            .setLore("§7Vous êtes dans l'équipe bleue")
+                            .build();
                     player.getInventory().setItem(0, blueWoolItem);
                     player.updateInventory();
                     
@@ -142,5 +116,4 @@ public class InteractListener implements Listener {
             }
         }
     }
-
 }
